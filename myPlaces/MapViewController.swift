09a315fbe19f217bcac29bb1230923e981e0d1a7
Artyom Khrebtov <t.khrebtov@yandex.ru>
2021-06-29 21:35:36 +0500

    add map hints

--- myPlaces/MapViewController.swift
+++ myPlaces/MapViewController.swift
@@ -7,25 +7,50 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     var coctail = Coctail()
     let annotationIdentifire = "annotationIdentifire"
+    let locationManager = CLLocationManager()
+    let regionInMeters = 10_000.00
+    var incomeSegueIdentifier = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupPlacemark()
-
+        setupMapView()
+        checkLocationAuth()
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBAction func doneButtonPressed() {
+        
+    }
+    
+    @IBAction func centerViewInUserLocation() {
+        showUserLocation()
+        
+    }
+    
+    @IBOutlet weak var mapPinImage: UIImageView!
     @IBAction func closeVC() {
         dismiss(animated: true)
     }
-   
+    
+    private func setupMapView() {
+        if incomeSegueIdentifier == "showPlace"{
+            setupPlacemark()
+            mapPinImage.isHidden = true
+            adressLabel.isHidden = true
+            doneButton.isHidden = true
+        }
+    }
+    
     private func setupPlacemark() {
         guard let location = coctail.ingridients else { return }
         let geocoder = CLGeocoder()
@@ -49,14 +74,70 @@ class MapViewController: UIViewController {
         }
         
     }
-
+    private func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuth()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(title: "Location Services are Disable",
+                               message: "To anable it go: Settings -> Privacy -> Location Services and turn On")
+            }
+        }
+    }
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+    }
+    private func checkLocationAuth() {
+        let manager = CLLocationManager()
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            if incomeSegueIdentifier == "getAdress" {
+                showUserLocation()
+            }
+            break
+        case .denied:
+            //AlertController
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //Alert
+            break
+        case .authorizedAlways:
+            break
+            
+        @unknown default:
+            print("new case is availible")
+        }
+        
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func showUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard !(annotation is MKUserLocation) else { return nil }
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifire)
-        as? MKPinAnnotationView
+            as? MKPinAnnotationView
         
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifire)
@@ -68,11 +149,17 @@ extension MapViewController: MKMapViewDelegate {
             imageView.layer.cornerRadius = 25
             imageView.clipsToBounds = true
             imageView.image = UIImage(data: imageData)
-           
+            
             annotationView?.rightCalloutAccessoryView = imageView
         }
         return annotationView
     }
     
     
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuth()
+    }
 }
